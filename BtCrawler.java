e1ff6aff01a885e9676c733b9df188cdfeb75f81
unknown <olivo@cs.utexas.edu>
2015-11-01 17:40:49 -0600

    Initial release of BluPhish

---
+++ BtCrawler.java
@@ -0,0 +1,135 @@
+/*
+	Copyright 2015, Oswaldo Olivo
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+
+	You may obtain a copy of the License at
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+import com.intel.bluetooth.*;
+import java.io.*;
+import java.util.*;
+import javax.bluetooth.*;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
+import javax.obex.*;
+
+// Class that processes discovered Bluetooth devices and services and displays diagnostic information.
+class BtCrawler {
+
+    static final Vector btDevices = new Vector();
+    static final Object inquiryCompletedEvent = new Object();
+    static final Object serviceSearchCompletedEvent = new Object();
+
+    static final HashMap<RemoteDevice, HashSet<ServiceRecord>> btDevice2Service = new HashMap<RemoteDevice, HashSet<ServiceRecord>>();
+
+    // Starts Bluetooth device discovery process.
+    public static void discoverDevices(BtDiscoveryListener listener) {
+
+	try {
+	    synchronized(inquiryCompletedEvent) {
+
+		boolean started = LocalDevice.getLocalDevice().getDiscoveryAgent().
+		    startInquiry(DiscoveryAgent.GIAC, listener);
+
+		if (started) {
+		    inquiryCompletedEvent.wait();
+		}
+	    }
+	}
+	catch (Exception e) { 
+	    System.out.println("ERROR: Could not start Bluetooth discovery.");
+	    System.out.println("Exception: " + e);
+	}
+    }
+
+    // Starts Bluetooth service discovery process.
+    public static void discoverServices(BtDiscoveryListener listener) {
+
+	// Searching for the Public Browse Group: all public services 
+	// on a device.
+	javax.bluetooth.UUID[] searchUuidSet = 
+	    new javax.bluetooth.UUID[] { new javax.bluetooth.UUID(0x1002) };
+
+	// Extracting the name attribute of the service, apart from the default attributes.
+	int[] attrIDSet = new int[] { 0x0100 };
+	
+	// Searching services for each discovered device.
+	for(Enumeration en = btDevices.elements(); en.hasMoreElements(); ) {
+	    try {
+	           RemoteDevice btDevice = (RemoteDevice)en.nextElement();
+		   synchronized (serviceSearchCompletedEvent) {
+
+		       LocalDevice.getLocalDevice().getDiscoveryAgent()
+		       .searchServices(attrIDSet, searchUuidSet, btDevice, listener);
+		       serviceSearchCompletedEvent.wait();
+		   }
+	    }
+	    catch (Exception e) {}
+	}
+    }
+
+    public static void main(String[] args) throws Exception {
+
+	// Sidestep compliance with parts of the L2CAP protocol.
+	//	System.setProperty(BlueCoveConfigProperties.PROPERTY_JSR_82_PSM_MINIMUM_OFF, "true");
+
+	BtDiscoveryListener listener = new BtDiscoveryListener();
+
+	discoverDevices(listener);
+	discoverServices(listener);
+
+	System.out.print("==============================================================");
+	System.out.println("==============================================================");
+	System.out.println("Device Name | Service Name | Authentication? | Encryption? | Connection URL | Vulnerable Connection?");
+	System.out.print("==============================================================");
+	System.out.println("==============================================================");
+
+	for (RemoteDevice btDevice : btDevice2Service.keySet()) {
+
+	    String deviceName = btDevice.getFriendlyName(false);
+
+	    for (ServiceRecord servRecord : btDevice2Service.get(btDevice)) {
+
+		String serviceName = servRecord.getAttributeValue(0x0100) != null ? 
+		    ((Object)servRecord.getAttributeValue(0x0100)).toString() : "";
+
+		String connectionURL = servRecord.getConnectionURL(ServiceRecord.NOAUTHENTICATE_NOENCRYPT, false);
+
+		boolean isAuthenticated = true;
+		boolean isEncrypted = true;
+		boolean isVulnerableConnection = false;
+
+		// Connecting to the device using the OBEX protocol.
+		if (connectionURL != null) {
+		
+		    isAuthenticated = false;
+		    isEncrypted = false;
+		    
+		    // Trying to connect to determine if the connection doesn't require pairing (vulnerable).
+		    try {
+
+			//			ClientSession clientSession = (ClientSession)Connector.open(connectionURL);
+			isVulnerableConnection = true;
+		    }
+		    catch (Exception e) {}
+		}
+		// Print diagnostic information.
+		System.out.print("==============================================================");
+		System.out.println("==============================================================");
+		System.out.print(deviceName + " | " + serviceName + " | " + isAuthenticated + " | ");
+		System.out.println(isEncrypted + " | " + connectionURL + " | " + isVulnerableConnection);
+		System.out.print("==============================================================");
+		System.out.println("==============================================================");
+	    }
+	}
+    }
+}
---
+++ BtDiscoveryListener.java
@@ -0,0 +1,64 @@
+/*
+	Copyright 2015, Oswaldo Olivo
+
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+
+	You may obtain a copy of the License at
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+import java.io.*;
+import java.util.*;
+import javax.bluetooth.*;
+
+// Class that discovers Bluetooth devices and services.
+class BtDiscoveryListener implements DiscoveryListener {
+
+    // Adds a discovered device to the global set of devices. 
+    public void deviceDiscovered(RemoteDevice btDevice, DeviceClass cod) {
+
+		    BtCrawler.btDevices.add(btDevice);
+    }
+
+    // Notifies all event listeners that device discovery is complete.
+    public void inquiryCompleted(int discType) {
+
+	synchronized(BtCrawler.inquiryCompletedEvent) {
+	    BtCrawler.inquiryCompletedEvent.notifyAll();
+	}
+    }
+
+    // Notifies all event listeners that service search is complete.
+    public void serviceSearchCompleted(int transID, int respCode) {
+
+	synchronized(BtCrawler.serviceSearchCompletedEvent) {
+	    BtCrawler.serviceSearchCompletedEvent.notifyAll();
+	}
+    }
+
+    // Stores discovered service information into the global map containing device-to-service information.
+    public void servicesDiscovered(int transID, ServiceRecord[] servRecord) {
+
+		    for(int i = 0; i < servRecord.length; i++) {
+
+			DataElement serviceName = servRecord[i].getAttributeValue(0x0100);
+			
+			// Get the device that offers the service.
+			RemoteDevice btDevice = servRecord[i].getHostDevice();
+			    
+			// Store the service information for the device.
+			if (!BtCrawler.btDevice2Service.containsKey(btDevice)) {
+			    BtCrawler.btDevice2Service.put(btDevice, new HashSet<ServiceRecord>());
+			}
+			
+			BtCrawler.btDevice2Service.get(btDevice).add(servRecord[i]);
+		    }
+		}
+}
